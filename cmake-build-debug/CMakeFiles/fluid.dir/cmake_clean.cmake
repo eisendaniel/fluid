f92e6585d03fde42dead455a06d81c13b3e954f9
eisendaniel <danieleisen99@gmail.com>
2019-11-27 18:50:06 +1300

    Init

---
+++ cmake-build-debug/CMakeFiles/fluid.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fluid.dir/main.cpp.obj"
+  "fluid.exe"
+  "fluid.exe.manifest"
+  "fluid.pdb"
+  "libfluid.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fluid.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
